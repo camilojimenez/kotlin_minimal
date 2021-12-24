5a21db9baf16fffebea3289411a9864cefc650b1
Camilo <camilo.jimenezc@gmail.com>
2021-12-23 21:44:20 -0500

    add all files

---
+++ app/src/main/java/com/example/todolist2/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.todolist2
+
+import android.app.AlertDialog
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        addBtn.setOnClickListener {
+            val dialogBuilder = AlertDialog.Builder(this)
+
+            // create dialog box
+            val alert = dialogBuilder.create()
+            // set title for alert dialog box
+            alert.setTitle("AlertDialogExample")
+            // show alert dialog
+            alert.show()
+        }
+
+    }
+}
